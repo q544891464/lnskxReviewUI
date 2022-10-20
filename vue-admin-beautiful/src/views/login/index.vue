96342f70167529b8412de6f09f48e242494ec41a
q544891464 <weifanchen1997@gmail.com>
2022-10-20 09:29:33 +0800

    标题修改省科协评奖

--- vue-admin-beautiful/src/views/login/index.vue
+++ vue-admin-beautiful/src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-<!--    <el-alert 默认带的广告栏 关闭了
+    <!--    <el-alert 默认带的广告栏 关闭了
       v-if="nodeEnv !== 'development'"
       title="beautiful boys and girls 欢迎加入 OPSLI 快速开发平台 QQ群：724850675 | API接口/数据库监控密码均为 admin : 123456 "
       type="success"
@@ -8,9 +8,8 @@
       style="position: fixed"
     ></el-alert>  -->
 
-
     <el-row>
-     <el-col :xs="24" :sm="24" :md="12" :lg="6" :xl="6">
+      <el-col :xs="24" :sm="24" :md="12" :lg="6" :xl="6">
         <div style="color: transparent">占位符</div>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
@@ -21,9 +20,9 @@
           class="login-form"
           label-position="left"
         >
-        <div class="title">
-            <img src="~@/assets/login_images/title.png" alt="">
-          </div>
+          <!-- <div class="title">
+            <img src="~@/assets/login_images/title.png" alt="" />
+          </div> -->
           <div class="title-tips">{{ title }}</div>
           <el-form-item style="margin-top: 40px" prop="username">
             <span class="svg-container svg-container-admin">
@@ -88,298 +87,294 @@
             登录
           </el-button>
 
-                  <router-link to="/register">
-                     <div style="margin-top: 20px">注册</div>
-                   </router-link>
+          <router-link to="/register">
+            <div style="margin-top: 20px">注册</div>
+          </router-link>
         </el-form>
       </el-col>
     </el-row>
 
     <!-- <login-tips ref="login-tips" @tipsClick="tipsClick"></login-tips> -->
-
   </div>
 </template>
 
 <script>
-  import { uuid } from "@/utils";
-  import { isNull } from "@/utils/valiargs";
-  import { validatorRule } from "@/utils/validateRlue";
-  import { captcha } from "@/api/user";
+import { uuid } from "@/utils";
+import { isNull } from "@/utils/valiargs";
+import { validatorRule } from "@/utils/validateRlue";
+import { captcha } from "@/api/user";
 
-  // TODO 演示专用 开发记得删除
-  // import LoginTips from "./components/LoginTips";
+// TODO 演示专用 开发记得删除
+// import LoginTips from "./components/LoginTips";
 
-  export default {
-    name: "Login",
-    directives: {
-      focus: {
-        inserted(el) {
-          el.querySelector("input").focus();
-        },
+export default {
+  name: "Login",
+  directives: {
+    focus: {
+      inserted(el) {
+        el.querySelector("input").focus();
       },
     },
-    // components: { LoginTips },
-    components: {  },
-    data() {
-
-      return {
-        nodeEnv: process.env.NODE_ENV,
-        title: this.$baseTitle,
-        form: {
-          username: "",
-          password: "",
-          captcha: "",
-          uuid: "",
-        },
-        rules: {
-          username: [
-            { required: true, trigger: "blur", message: "请输入用户名" },
-          ],
-          password: [
-            { required: true, trigger: "blur", message: "请输入密码" },
-            // { required: true, trigger: "blur", validator: validatorRule.IS_GENERAL },
-          ],
-          captcha: [
-            { required: true, trigger: "blur", message: "请输入验证码" },
-          ],
-        },
-        captchaFlag: false,
-        loading: false,
-        passwordType: "password",
-        redirect: undefined,
-        captchaImg: "",
-      };
-    },
-    watch: {
-      $route: {
-        handler(route) {
-          this.redirect = (route.query && route.query.redirect) || "/";
-        },
-        immediate: true,
+  },
+  // components: { LoginTips },
+  components: {},
+  data() {
+    return {
+      nodeEnv: process.env.NODE_ENV,
+      title: this.$baseTitle,
+      form: {
+        username: "",
+        password: "",
+        captcha: "",
+        uuid: "",
       },
+      rules: {
+        username: [
+          { required: true, trigger: "blur", message: "请输入用户名" },
+        ],
+        password: [
+          { required: true, trigger: "blur", message: "请输入密码" },
+          // { required: true, trigger: "blur", validator: validatorRule.IS_GENERAL },
+        ],
+        captcha: [{ required: true, trigger: "blur", message: "请输入验证码" }],
+      },
+      captchaFlag: false,
+      loading: false,
+      passwordType: "password",
+      redirect: undefined,
+      captchaImg: "",
+    };
+  },
+  watch: {
+    $route: {
+      handler(route) {
+        this.redirect = (route.query && route.query.redirect) || "/";
+      },
+      immediate: true,
     },
-    created() {
-      document.body.style.overflow = "hidden";
-      this.form.uuid = uuid();
+  },
+  created() {
+    document.body.style.overflow = "hidden";
+    this.form.uuid = uuid();
+    this.captchaImg = captcha(this.form.uuid);
+  },
+  beforeDestroy() {
+    document.body.style.overflow = "auto";
+  },
+  mounted() {
+    // TODO 演示使用 开发手动删除
+    // this.$refs["login-tips"].show();
+  },
+  methods: {
+    // TODO 演示使用 开发手动删除
+    tipsClick(ret) {
+      // 点击登录
+      this.form.username = ret.username;
+      this.form.password = ret.password;
+      this.handleLogin();
+    },
+    // 获得新验证码
+    getCaptcha() {
       this.captchaImg = captcha(this.form.uuid);
     },
-    beforeDestroy() {
-      document.body.style.overflow = "auto";
+    handleLogin() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          this.$store
+            .dispatch("user/login", this.form)
+            .then(() => {
+              const routerPath =
+                this.redirect === "/404" || this.redirect === "/401"
+                  ? "/"
+                  : this.redirect;
+              // this.$router.push(routerPath).catch(() => {});
+              this.$router.push("/index").catch(() => {});
+              this.loading = false;
+            })
+            .catch(() => {
+              // 获取当前失败次数
+              this.$store
+                .dispatch("user/getSlipCount", this.form)
+                .then((ret) => {
+                  this.loading = false;
+                  const { data } = ret;
+                  if (!isNull(data) && data.curr >= data.base) {
+                    // 失败次数大于系统规定阈值 开启验证码校验
+                    this.captchaFlag = true;
+                  }
+                })
+                .catch(() => {
+                  this.loading = false;
+                });
+            });
+        } else {
+          return false;
+        }
+      });
     },
-    mounted() {
-      // TODO 演示使用 开发手动删除
-      // this.$refs["login-tips"].show();
-    },
-    methods: {
-      // TODO 演示使用 开发手动删除
-      tipsClick(ret){
-        // 点击登录
-        this.form.username = ret.username;
-        this.form.password = ret.password;
-        this.handleLogin();
-      },
-      // 获得新验证码
-      getCaptcha() {
-        this.captchaImg = captcha(this.form.uuid);
-      },
-      handleLogin() {
-        this.$refs.form.validate((valid) => {
-          if (valid) {
-            this.loading = true;
-            this.$store
-              .dispatch("user/login", this.form)
-              .then(() => {
-                const routerPath =
-                  this.redirect === "/404" || this.redirect === "/401"
-                    ? "/"
-                    : this.redirect;
-                // this.$router.push(routerPath).catch(() => {});
-                this.$router.push("/index").catch(() => {});
-                this.loading = false;
-              })
-              .catch(() => {
-                // 获取当前失败次数
-                this.$store
-                  .dispatch("user/getSlipCount", this.form)
-                  .then((ret) => {
-                    this.loading = false;
-                    const { data } = ret;
-                    if (!isNull(data) && data.curr >= data.base) {
-                      // 失败次数大于系统规定阈值 开启验证码校验
-                      this.captchaFlag = true;
-                    }
-                  })
-                  .catch(() => {
-                    this.loading = false;
-                  });
-              });
-          } else {
-            return false;
-          }
-        });
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .login-container {
-    height: 100vh;
-    background: url("~@/assets/login_images/background2.jpg") center center fixed
-      no-repeat;
-    background-size: cover;
+.login-container {
+  height: 100vh;
+  background: url("~@/assets/login_images/background2.jpg") center center fixed
+    no-repeat;
+  background-size: cover;
 
-    .title {
-      font-size: 54px;
-      font-weight: 500;
-      color: rgba(14, 18, 26, 1);
+  .title {
+    font-size: 54px;
+    font-weight: 500;
+    color: rgba(14, 18, 26, 1);
+  }
+
+  .title-tips {
+    margin-top: 29px;
+    font-size: 25px;
+    font-weight: 400;
+
+    // color: rgba(14, 18, 26, 1);
+    color: #d7dee3;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .login-btn {
+    display: inherit;
+    width: 220px;
+    height: 60px;
+    margin-top: 5px;
+    border: 0;
+
+    &:hover {
+      opacity: 0.9;
     }
+  }
 
-    .title-tips {
-      margin-top: 29px;
-      font-size: 25px;
-      font-weight: 400;
+  .login-form {
+    background-color: #00000060;
+    position: relative;
+    max-width: 100%;
+    // margin: calc((100vh - 425px) / 2) 10% 10%;
+    margin: 10% 10%;
+    // margin: 50px 90px;
+    overflow: hidden;
+    padding: 2rem 4rem;
 
-      // color: rgba(14, 18, 26, 1);
-      color: #d7dee3;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
+    .forget-password {
+      width: 100%;
+      margin-top: 40px;
+      text-align: left;
 
-    .login-btn {
-      display: inherit;
-      width: 220px;
-      height: 60px;
-      margin-top: 5px;
-      border: 0;
-
-      &:hover {
-        opacity: 0.9;
-      }
-    }
-
-    .login-form {
-      background-color: #00000060;
-      position: relative;
-      max-width: 100%;
-      // margin: calc((100vh - 425px) / 2) 10% 10%;
-      margin: 10% 10%;
-      // margin: 50px 90px;
-      overflow: hidden;
-      padding: 2rem 4rem;
-
-      .forget-password {
-        width: 100%;
-        margin-top: 40px;
-        text-align: left;
-
-        .forget-pass {
-          width: 129px;
-          height: 19px;
-          font-size: 20px;
-          font-weight: 400;
-          color: rgba(92, 102, 240, 1);
-        }
-      }
-    }
-
-    .tips {
-      margin-bottom: 10px;
-      font-size: $base-font-size-default;
-      color: $base-color-white;
-
-      span {
-        &:first-of-type {
-          margin-right: 16px;
-        }
-      }
-    }
-
-    .title-container {
-      position: relative;
-
-      .title {
-        margin: 0 auto 40px auto;
-        font-size: 34px;
-        font-weight: bold;
-        color: $base-color-blue;
-        text-align: center;
-      }
-    }
-
-    .svg-container {
-      position: absolute;
-      top: 14px;
-      left: 15px;
-      z-index: $base-z-index;
-      font-size: 16px;
-      color: #d7dee3;
-      cursor: pointer;
-      user-select: none;
-    }
-
-    .show-password {
-      position: absolute;
-      top: 14px;
-      right: 25px;
-      font-size: 16px;
-      color: #d7dee3;
-      cursor: pointer;
-      user-select: none;
-    }
-
-    .captcha {
-      position: absolute;
-      top: -2.5px;
-      right: 0;
-      width: 180px;
-      height: 58px;
-      margin-top: 2px;
-      cursor: pointer;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-    }
-
-    ::v-deep {
-      .el-form-item {
-        padding-right: 0;
-        margin: 20px 0;
-        color: #454545;
-        background: transparent;
-        border: 1px solid transparent;
-        border-radius: 2px;
-
-        &__content {
-          min-height: $base-input-height;
-          line-height: $base-input-height;
-        }
-
-        &__error {
-          position: absolute;
-          top: 100%;
-          left: 18px;
-          font-size: $base-font-size-small;
-          line-height: 18px;
-          color: $base-color-red;
-        }
-      }
-
-      .el-input {
-        box-sizing: border-box;
-
-        input {
-          height: 58px;
-          padding-left: 45px;
-          font-size: $base-font-size-default;
-          line-height: 58px;
-          color: $base-font-color;
-          background: #f6f4fc;
-          border: 0;
-          caret-color: $base-font-color;
-        }
+      .forget-pass {
+        width: 129px;
+        height: 19px;
+        font-size: 20px;
+        font-weight: 400;
+        color: rgba(92, 102, 240, 1);
       }
     }
   }
+
+  .tips {
+    margin-bottom: 10px;
+    font-size: $base-font-size-default;
+    color: $base-color-white;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      margin: 0 auto 40px auto;
+      font-size: 34px;
+      font-weight: bold;
+      color: $base-color-blue;
+      text-align: center;
+    }
+  }
+
+  .svg-container {
+    position: absolute;
+    top: 14px;
+    left: 15px;
+    z-index: $base-z-index;
+    font-size: 16px;
+    color: #d7dee3;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .show-password {
+    position: absolute;
+    top: 14px;
+    right: 25px;
+    font-size: 16px;
+    color: #d7dee3;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .captcha {
+    position: absolute;
+    top: -2.5px;
+    right: 0;
+    width: 180px;
+    height: 58px;
+    margin-top: 2px;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  ::v-deep {
+    .el-form-item {
+      padding-right: 0;
+      margin: 20px 0;
+      color: #454545;
+      background: transparent;
+      border: 1px solid transparent;
+      border-radius: 2px;
+
+      &__content {
+        min-height: $base-input-height;
+        line-height: $base-input-height;
+      }
+
+      &__error {
+        position: absolute;
+        top: 100%;
+        left: 18px;
+        font-size: $base-font-size-small;
+        line-height: 18px;
+        color: $base-color-red;
+      }
+    }
+
+    .el-input {
+      box-sizing: border-box;
+
+      input {
+        height: 58px;
+        padding-left: 45px;
+        font-size: $base-font-size-default;
+        line-height: 58px;
+        color: $base-font-color;
+        background: #f6f4fc;
+        border: 0;
+        caret-color: $base-font-color;
+      }
+    }
+  }
+}
 </style>
